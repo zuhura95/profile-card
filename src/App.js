c9d8adf95af8dcb85c09b1ac7de160408a738690
zuhura <zuhura1995@gmail.com>
2021-01-22 23:15:56 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      followers: 1000,
+      likes: 803,
+      photos: 1400,
+      isFollowing: false
+    };
+  }
+  
+
+  addFollower(){
+    console.log('ok');
+  }
+  render(){
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className= "App">
+    <main>
+    <div className="card">
+      <header>
+        <div className="avatar">
+        </div>
+       
       </header>
+       <div className="content">
+       <h2>Victor Crest
+          <div className="age">26</div>
+        </h2> 
+        <div className="label">London</div>
+        
+        <button className="followButton"
+        onClick={this.addFollower}>Follow</button>
+        
+       </div>
+      
+        <footer>
+          <div className="col">
+                <h3>{this.state.followers}</h3>
+                <div className="statsLabel">Followers</div>
+          </div>
+          <div className="col">
+            <h3>{this.state.likes}</h3>
+            <div className="statsLabel">Likes</div>
+          </div>
+          <div className="col">
+            <h3>{this.state.photos}</h3>
+            <div className="statsLabel">Photos</div> 
+          </div>
+         
+        </footer>
     </div>
+  </main>
+  </div>
   );
+}
 }
 
 export default App;
